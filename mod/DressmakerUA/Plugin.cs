9107485e32a6e77c36680949ffde3d6cf0c65c44
nightsuperthunder <42420139+nightsuperthunder@users.noreply.github.com>
2026-09-22 22:39:38 +0300

    Fix seven more dialogue lines after community review; release 1.1.3

--- mod/DressmakerUA/Plugin.cs
+++ mod/DressmakerUA/Plugin.cs
@@ -31,7 +31,7 @@ namespace DressmakerUA
     public class Plugin : BaseUnityPlugin
     {
         public const string Guid = "ua.dressmaker.localization";
-        public const string Version = "1.1.2";
+        public const string Version = "1.1.3";
 
         internal static ManualLogSource Log;
         internal static Plugin Instance;

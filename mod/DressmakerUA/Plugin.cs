f352c2ed6446743e87d854264a120962bba894e7
nightsuperthunder <42420139+nightsuperthunder@users.noreply.github.com>
2026-09-22 22:24:17 +0300

    Bump plugin version to 1.1.2

--- mod/DressmakerUA/Plugin.cs
+++ mod/DressmakerUA/Plugin.cs
@@ -31,7 +31,7 @@ namespace DressmakerUA
     public class Plugin : BaseUnityPlugin
     {
         public const string Guid = "ua.dressmaker.localization";
-        public const string Version = "1.1.1";
+        public const string Version = "1.1.2";
 
         internal static ManualLogSource Log;
         internal static Plugin Instance;
